7bcd89e2e985789ce38690ea38aee502b36bbbef
Justin Pennington <justin_pennington@onlifehealth.com>
2025-06-05 11:21:43 -0500

    update treesitter source and after script with new reference

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,8 @@
 local M = {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })
-    end,
+    build = ':TSUpdate',
+    branch = 'main'
 }
 
 return { M }
